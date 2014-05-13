ea1b0fa936789aee6d4024393585cf84f1ee4f87
dondi <dondi@lmu.edu>
2014-05-13 01:11:04 -0700

    Inline comments and feedback for HW 0422.

--- homework/address-translation/addressTranslation.c
+++ homework/address-translation/addressTranslation.c
@@ -14,8 +14,11 @@ void setPageTable(pagetable *pt) {
 
 int getPhysical(int logical) {
 	int four_bits_farthest_left = ((logical & PAGEMASK) >> PAGEBITS);
+    // JD: Note how the bitwise-"&" is^^^^^^^^ unnecessary because of the right shift.
 	int four_bits_farthest_right = (logical & PAGESIZE);
 
+    // JD: This max check can be unhardcoded through got use of
+    //     some #define's in addressTranslation.h
 	if (logical < 0 || logical >= 256) {
         return ERR_OUT_OF_RANGE;
     } else if (ptr[four_bits_farthest_left].valid == 0) {
